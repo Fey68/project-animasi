13c9770cd8956b1aefa188a38cb1740d24046821
Fey68 <58196165+Fey68@users.noreply.github.com>
2019-11-27 16:30:22 +0700

    Initial commit

---
+++ Controllers/Moviescontroller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class MovieController : Controller
+    {
+        //Get: movie/random
+        public ActionResult random()
+        {
+            var movie = new Movie() { Name = "Shrek!" , Id= 5 };
+            return View(movie);
+        }
+
+    }
+}
